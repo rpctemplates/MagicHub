c5cf5757a271e80b1caadc6cef37aa1721e80b8d
NotMagicHub <ItzzzzMagicccc@Gmail.com>
2026-02-04 15:39:05 -0500

    Update Safety.cs

--- iis.Stupid.Template-1.9.5/iis.Stupid.Template-1.9.5/Mods/Safety.cs
+++ iis.Stupid.Template-1.9.5/iis.Stupid.Template-1.9.5/Mods/Safety.cs
@@ -1,49 +1,51 @@
-﻿using GorillaLocomotion;
-using StupidTemplate.Classes;
-using StupidTemplate.Notifications;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.XR;
-using static StupidTemplate.Classes.RigManager;
-using static StupidTemplate.Menu.Main;
+using System;
 
-namespace StupidTemplate.Mods
+public class SafetyMods : MonoBehaviour
 {
-    public class Safety
+    // ---------------- STATES ----------------
+    public bool autoLeaveOnModerator;
+    public bool autoLeaveOnOwner;
+    public bool panicDisconnect;
+
+    // ---------------- EVENTS (SIMULATED) ----------------
+    // These would normally come from YOUR networking code
+    public event Action<PlayerInfo> OnPlayerJoined;
+
+    void Start()
     {
-        public static VRRig reportRig;
-        public static void AntiReport(System.Action<VRRig, Vector3> onReport)
+        OnPlayerJoined += HandlePlayerJoin;
+    }
+
+    void Update()
+    {
+        if (panicDisconnect)
         {
-            if (!NetworkSystem.Instance.InRoom) return;
-
-            if (reportRig != null)
-            {
-                onReport?.Invoke(reportRig, reportRig.transform.position);
-                reportRig = null;
-                return;
-            }
-
-            foreach (GorillaPlayerScoreboardLine line in GorillaScoreboardTotalUpdater.allScoreboardLines)
-            {
-                if (line.linePlayer != NetworkSystem.Instance.LocalPlayer) continue;
-                Transform report = line.reportButton.gameObject.transform;
-
-                foreach (var vrrig in from vrrig in GorillaParent.instance.vrrigs where !vrrig.isLocal let D1 = Vector3.Distance(vrrig.rightHandTransform.position, report.position) let D2 = Vector3.Distance(vrrig.leftHandTransform.position, report.position) where D1 < 0.35f || D2 < 0.35f select vrrig)
-                    onReport?.Invoke(vrrig, report.transform.position);
-            }
-        }
-
-        public static float antiReportDelay;
-        public static void AntiReportDisconnect()
-        {
-            AntiReport((vrrig, position) =>
-            {
-                NetworkSystem.Instance.ReturnToSinglePlayer();
-
-                if (!(Time.time > antiReportDelay)) return;
-                antiReportDelay = Time.time + 1f;
-                NotifiLib.SendNotification("<color=grey>[</color><color=purple>ANTI-REPORT</color><color=grey>]</color> " + GetPlayerFromVRRig(vrrig).NickName + " attempted to report you, you have been disconnected.");
-            });
+            Disconnect();
+            panicDisconnect = false;
         }
     }
+
+    void HandlePlayerJoin(PlayerInfo player)
+    {
+        if (autoLeaveOnModerator && player.isModerator)
+            Disconnect();
+
+        if (autoLeaveOnOwner && player.isRoomOwner)
+            Disconnect();
+    }
+
+    void Disconnect()
+    {
+        Debug.Log("Safety Mod: Leaving room");
+        // Replace with YOUR networking disconnect
+    }
+}
+
+// ---------------- SUPPORT CLASS ----------------
+public class PlayerInfo
+{
+    public string playerName;
+    public bool isModerator;
+    public bool isRoomOwner;
 }
